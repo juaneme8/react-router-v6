1b6a2f266dc2ca42e409cb1d89e4616505f5f806
Juan Ocho <juaneme8@gmail.com>
2022-08-17 14:51:05 -0300

    Notes about new version

---
+++ src/pages/Error.js
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+    <section>
+      <h2>404</h2>
+      <p>Page not found</p>
+    </section>
+  )
+}
+
+export default Error;
---
+++ src/pages/Products.js
@@ -0,0 +1,9 @@
+const Products = () => {
+  return (
+    <section>
+      <h2>Products Page</h2>
+    </section>
+  )
+}
+
+export default Products;
